c0238b745cdf79cf8e7dbaeaaf2c1a90d197eac4
j4dev <jesusguanga98@outlook.com>
2019-11-16 01:46:11 -0500

    Create components and add angular-material.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/users/login/login.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: '**', component: PagenotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  AngularMaterialModule } from './angular-material.module';
+
+import { LoginComponent } from './components/users/login/login.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { InicioComponent } from './components/users/inicio/inicio.component';
+import { PerfilComponent } from './components/users/perfil/perfil.component';
+import { EditarPerfilComponent } from './components/users/editar-perfil/editar-perfil.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { BodegasComponent } from './components/bodegas/bodegas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagenotfoundComponent,
+    InicioComponent,
+    PerfilComponent,
+    EditarPerfilComponent,
+    ProductosComponent,
+    BodegasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
